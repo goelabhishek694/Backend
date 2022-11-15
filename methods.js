5c8ca0d2801b5462d8e1c6e19d1b90b801573027
goelabhishek694 <goelabhishek694@gmail.com>
2022-11-15 21:43:53 +0530

    :construction: query & params

--- methods.js
+++ methods.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-let user = {};
-
+let user = [
+  {
+    id: 1,
+    name: "Abhishek",
+    age: 100,
+  },
+  {
+    id: 2,
+    name: "Rajat",
+    age: 10,
+  },
+  {
+    id: 3,
+    name: "Sunjyot",
+    age: 50,
+  },
+];
+//with query
 app.get('/user', (req, res) => {
-    res.send(user);
+    console.log(req.query);
+    let { name, age } = req.query;
+    let filteredData=user.filter(userObj => {
+        return (userObj.name==name && userObj.age==age)
+    })
+    res.send(filteredData);
 })
 
 app.post('/user', (req, res) => {
@@ -34,6 +55,14 @@ app.delete('/user', (req, res) => {
         msg: "user has been deleted"
     });
 })
+
+//params
+app.get('/user/:name', (req, res) => {
+    console.log(req.params.name);
+    //let {id}=req.params;
+    // let user = db.findOne(id);
+    res.json({ msg: "user id is ", "obj": req.params });
+});
 
 
 
